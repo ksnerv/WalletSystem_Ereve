adbbad1c3952485156e455c0c36cef3d7dff0d6c
ksnerv <76584180+ksnerv@users.noreply.github.com>
2022-03-05 12:12:01 +0800

    Can now register new accounts

--- WalletSystem_Ereve_Source/WalletSystem_Ereve_Source/Class/BusinessLogicLayer/User_BLL.cs
+++ WalletSystem_Ereve_Source/WalletSystem_Ereve_Source/Class/BusinessLogicLayer/User_BLL.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 using WalletSystem_Ereve_Source.Class.DataAccessLayer;
 using WalletSystem_Ereve_Source.Class.Model;
@@ -38,6 +39,37 @@ namespace WalletSystem_Ereve_Source.Class.BusinessLogicLayer
 
             return (list.Count > 0) ? list : null;
 
+        }
+
+        internal static bool SaveUser(User newUser)
+        {
+            using (DAL dal = new DAL())
+            {
+                if (!dal.IsConnected) return false;
+
+
+                SqlParameter[] param = { new  SqlParameter("@account_num",newUser.accountNumber),
+                                         new  SqlParameter("@name", newUser.name),
+                                         new  SqlParameter("@password", newUser.password),
+                                         new  SqlParameter("@date", newUser.registerDate)
+
+                                       };
+
+                try
+                {
+                    dal.ExecuteNonQuery("SaveUser", param);
+                    return true; //saved successfully
+                }
+                catch (Exception ex)
+                {
+                    //inspect ex.Message
+                    string error = ex.Message;
+                    return false;
+                }
+
+
+
+            }
         }
     }
 }
--- WalletSystem_Ereve_Source/WalletSystem_Ereve_Source/Program.cs
+++ WalletSystem_Ereve_Source/WalletSystem_Ereve_Source/Program.cs
@@ -1,19 +1,75 @@
 ﻿using System;
+using System.Collections.Generic;
 using WalletSystem_Ereve_Source.Class.BusinessLogicLayer;
+using WalletSystem_Ereve_Source.Class.Model;
 
 namespace WalletSystem_Ereve_Source
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
-            var users = User_BLL.GetAllUsers();
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("Wallet System\n");
+                Console.WriteLine("Press [1] Login");
+                Console.WriteLine("Press [2] Register");
 
+                Console.WriteLine("Press [0] Exit");
+                var input = Console.ReadKey();
+                Console.Clear();
+                switch(input.KeyChar)
+                {
+                    case '1': Login();
+                        break;
+                    case '2': Register();
+                        break;
+                    case '0': return;
+                      
+                }
+
+                
+
+            }
+
+        }
+
+        private static void Register()
+        {
+            Console.WriteLine("Wallet System\n");
+            Console.WriteLine("Register new Account\n");
+            Console.Write("Username: ");
+            var name = Console.ReadLine();
+            Console.Write("Password: ");
+            var password = Console.ReadLine();
+
+            var newUser = new User()
+            {
+                name = name,
+                password = password,
+                registerDate = DateTime.Now
+            };
+
+            if(User_BLL.SaveUser(newUser))
+            {
+                //saved successfully
+                Console.WriteLine("Saved Successfully!!");
+                
+            }
+            else
+            {
+                Console.WriteLine("Error saving!");
+            }
             Console.ReadKey();
+            
+        }
 
-
+        private static void Login()
+        {
+            throw new NotImplementedException();
         }
     }
 }
